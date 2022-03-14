5c5a7e6e178296f21034de3c95739b8479a2773f
Fenyvesi Mátyás <43251500+fenyvesimate211@users.noreply.github.com>
2022-03-14 16:36:03 +0100

    userRegister button opens userProfile

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,4 +1,45 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.Objects;
+
 public class LoginController {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Label wrongLogin;
+    @FXML
+    private Button registerButton;
+
+    public LoginController() {
+
+    }
+
+    public void userLogin(ActionEvent actionEvent) throws IOException {
+
+    }
+
+    public void userRegister (ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/userprofile.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
